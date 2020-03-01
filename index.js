5de2298a965324b1d8e40484baeba7708da42cb5
tarateo78 <tarateo78@gmail.com>
2020-03-01 21:46:03 +0100

    ident

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const http = require('http');
 
-
-
 const PORT =  process.env.PORT || 3000;
 
-console.log( PORT );
+const ident = process.env.ident || "i";
+const pwd = process.env.pwd || "p";
 
 http.createServer( (req, res) => {
 	if( req.url === '/' ) {
+// da qui
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// a qui
 		res.write("eccolo!");
 		res.end();
 		console.log( 'ecco' );
 		console.log( PORT );
+		console.log( ident, pwd );
 	}
 }).listen(PORT);
+
