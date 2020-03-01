7d88c0b6b82b8f9295a8c16a6040b2c355bf02de
tarateo78 <tarateo78@gmail.com>
2020-03-01 20:57:10 +0100

    port

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const http = require('http');
 
 console.log( 'ecco' );
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer( function( req, res ) {
+	res.writeHead( 200, { 'Content-Type' : 'text/html'} );
+	res.end( creditoTmp[0] );
+}).listen(PORT);
